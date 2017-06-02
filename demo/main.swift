4b503558a74d47ebc0f3186a56f49fe2893031a4
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2017-06-02 10:54:46 -0400

    Initial Commit

---
+++ demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  demo
+//
+//  Created by Nicholas Mosier on 6/2/17.
+//  Copyright © 2017 Nicholas Mosier. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
