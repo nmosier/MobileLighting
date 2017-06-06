fe481d0f415bb2b81886e0172d0bf56f5b6a251b
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2017-06-06 08:26:02 -0400

    CameraInstructionPacket: 	- added optional pointOfFocus property for changing focus (CGPoint)

--- CameraInstructionPacket.swift
+++ CameraInstructionPacket.swift
@@ -15,6 +15,8 @@ class CameraInstructionPacket: NSObject, NSCoding {
     var captureSessionPreset: String!   // for setting photo resolution (use constants "AVCaptureSessionPreset...")
     var photoBracketExposures: [Double]?   // optional b/c only used for bracketed photo sequences -> in seconds
                                             // implicitly contains number of photos in bracket (= # items in array)
+    var pointOfFocus: CGPoint?  // point of focus: represents where to focus in field of view
+    
     //MARK: Initialization
     
     // for decoding packet
@@ -23,14 +25,16 @@ class CameraInstructionPacket: NSObject, NSCoding {
         self.cameraInstruction = CameraInstruction(rawValue: decoder.decodeInteger(forKey: "cameraInstruction"))
         self.captureSessionPreset = decoder.decodeObject(forKey: "captureSessionPreset") as! String!
         self.photoBracketExposures = decoder.decodeObject(forKey: "photoBracketExposures") as! [Double]?
+        self.pointOfFocus = decoder.decodeObject(forKey: "pointOfFocus") as! CGPoint?
     }
     
     // for standard initialization
-    convenience init(cameraInstruction: CameraInstruction, captureSessionPreset: String = AVCaptureSessionPresetPhoto, photoBracketExposures: [Double]? = nil) {
+    convenience init(cameraInstruction: CameraInstruction, captureSessionPreset: String = AVCaptureSessionPresetPhoto, photoBracketExposures: [Double]? = nil, pointOfFocus: CGPoint? = nil) {
         self.init()
         self.cameraInstruction = cameraInstruction
         self.captureSessionPreset = captureSessionPreset
         self.photoBracketExposures = photoBracketExposures
+        self.pointOfFocus = pointOfFocus
     }
     
     //MARK: Encoding/decoding
@@ -38,6 +42,7 @@ class CameraInstructionPacket: NSObject, NSCoding {
         coder.encode(self.cameraInstruction.rawValue, forKey: "cameraInstruction")
         coder.encode(self.captureSessionPreset, forKey: "captureSessionPreset")
         coder.encode(self.photoBracketExposures, forKey: "photoBracketExposures")
+        coder.encode(self.pointOfFocus, forKey: "pointOfFocus")
     }
     
 }
