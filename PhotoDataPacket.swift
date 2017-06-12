3b6a2f9f21e198cf82779fc1add6aded1c41d2bc
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2017-06-12 09:02:51 -0400

    Added enum CameraStatusUpdate for photo packet (defaults to .None in std. initializer; has case to indicate finished capturing normal binary code image bracket

--- PhotoDataPacket.swift
+++ PhotoDataPacket.swift
@@ -8,9 +8,15 @@
 
 import Foundation
 
+enum CameraStatusUpdate: Int {
+    case None
+    case CapturedNormalBinaryCode
+}
+
 @objc(PhotoDataPacket)
 class PhotoDataPacket: NSObject, NSCoding {    
     //MARK: Properties
+    var statusUpdate: CameraStatusUpdate!
     var encounteredError: Bool!
     var photoData: Data!
     var bracketedPhotoID: Int?
@@ -20,14 +26,16 @@ class PhotoDataPacket: NSObject, NSCoding {
     required convenience init?(coder decoder: NSCoder) {
         self.init()
         // decode data using key "photoData"
+        self.statusUpdate = CameraStatusUpdate(rawValue: decoder.decodeInteger(forKey: "statusUpdate"))
         self.encounteredError = decoder.decodeObject(forKey: "encounteredError") as! Bool
         self.photoData = decoder.decodeObject(forKey: "photoData") as? Data
         self.bracketedPhotoID = decoder.decodeObject(forKey: "bracketedPhotoID") as! Int?
         self.lensPosition = decoder.decodeObject(forKey: "lensPosition") as! Float?
     }
     
-    convenience init(photoData: Data, bracketedPhotoID: Int? = nil, lensPosition: Float? = nil) {
+    convenience init(photoData: Data, bracketedPhotoID: Int? = nil, lensPosition: Float? = nil, statusUpdate: CameraStatusUpdate = .None) {
         self.init()
+        self.statusUpdate = statusUpdate
         self.encounteredError = false
         self.photoData = photoData
         self.bracketedPhotoID = bracketedPhotoID
@@ -43,6 +51,7 @@ class PhotoDataPacket: NSObject, NSCoding {
     
     //MARK: encoding/decoding
     func encode(with coder: NSCoder) {
+        coder.encode(statusUpdate.rawValue, forKey: "statusUpdate")
         coder.encode(encounteredError, forKey: "encounteredError")
         coder.encode(photoData, forKey: "photoData")
         coder.encode(bracketedPhotoID, forKey: "bracketedPhotoID")
