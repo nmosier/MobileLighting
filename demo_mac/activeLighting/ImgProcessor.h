794acc56187cd3705bb05336180ad0b045768f52
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2017-07-17 15:29:19 -0400

    Changed settings around.

--- demo_mac/activeLighting/ImgProcessor.h
+++ demo_mac/activeLighting/ImgProcessor.h
@@ -1,7 +1,7 @@
 // Header file for selected image processing code from ActiveLighting
 // created 06/28/2017 by Nicholas Mosier
 
-#include "imageLib.h"
+#include "imageLib/imageLib.h"
 #include "flowIO.h"
 #include "Utils.h"
 
