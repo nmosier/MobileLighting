6c3273dc9f9adcf294acbc27a04fdc3c281b5be9
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2018-06-15 16:38:47 -0400

    added wrapper functions for robot arm control

--- MobileLighting_Mac/RobotControl/RobotControl/RobotControl.cpp
+++ MobileLighting_Mac/RobotControl/RobotControl/RobotControl.cpp
@@ -7,17 +7,36 @@
 //
 
 #include <iostream>
-#include "RobotControl.hpp"
+//#include "RobotControl.h"
+#include "MovementControl.h"
 
 extern "C" int Client() {
     return client();
-}
-extern "C" int Server() {
-    return server();
 }
 extern "C" int Restore() {
     return restore();
 }
 extern "C" int Next() {
     return next();
+}
+extern "C" int PowerdownRobot() {
+    return powerdown();
+}
+extern "C" int MovePose(char *pose, float a, float v) {
+    return move_pose(std::string(pose), a, v);
+}
+extern "C" int MoveJoints(char *pose, float a, float v) {
+    return move_joints(std::string(pose), a, v);
+}
+extern "C" int MoveLinearX(float d, float a, float v) {
+    if (a == 0 || v == 0) return linear_x(d);
+    else return linear_x(d, a, v);
+}
+extern "C" int MoveLinearY(float d, float a, float v) {
+    if (a == 0 || v == 0) return linear_y(d);
+    else return linear_y(d, a, v);
+}
+extern "C" int MoveLinearZ(float d, float a, float v) {
+    if (a == 0 || v == 0) return linear_z(d);
+    else return linear_z(d, a, v);
 }
