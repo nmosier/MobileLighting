e3aea761a5a45e9063e1aee06612dcbd7b34132d
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2018-06-15 16:27:30 -0400

    added CalibrationSettings class

--- MobileLighting_Mac/SettingsManager.swift
+++ MobileLighting_Mac/SettingsManager.swift
@@ -30,7 +30,7 @@ class InitSettings {
     var nProjectors: Int?
     var exposureDurations: [Double]?
     var exposureISOs: [Double]?
-    var positionCoords: [Int]?
+    var positionCoords: [String]?
 }
 
 // SceneParameters: will represent the parameters determined during and after scene capture,
@@ -43,9 +43,8 @@ class InitSettings {
 class SceneParameters {
     var sceneName: String!
     var nPositions: Int { get { return self.positions.count } }
-    var positions: [Int]!
+    var positions: [String]!
     var structuredLighting: StructuredLightingParameters!
-    var calibration: CalibrationParameters!
 }
 
 class StructuredLightingParameters {
@@ -56,10 +55,6 @@ class StructuredLightingParameters {
     var exposureISOs: [Double]!
     var lensPosition: Float!
     var focusPoint: CGPoint?
-}
-
-class CalibrationParameters {
-    
 }
 
 
@@ -109,10 +104,120 @@ func loadInitSettings(filepath: String) throws -> InitSettings {
         (val: Yaml) -> Double in
         return val.double!
     }
-    initSettings.positionCoords = mainDict[Yaml.string("positions")]?.array?.filter({return $0.int != nil}).map{
-        (val: Yaml) -> Int in
-        return val.int!
+    initSettings.positionCoords = mainDict[Yaml.string("positions")]?.array?.filter({return $0.string != nil}).map{
+        (val: Yaml) -> String in
+        return val.string!
     }
     
     return initSettings
+}
+
+
+func generateIntrinsicsImageList(imgsdir: String = dirStruc.intrinsicsPhotos, outpath: String = dirStruc.calibrationSettings + "/intrinsicsImageList.yml") {
+    guard var imgs = try? FileManager.default.contentsOfDirectory(atPath: imgsdir) else {
+        print("could not read contents of directory \(imgsdir)")
+        return
+    }
+    imgs = imgs.filter { (_ filepath: String) in
+        let file = filepath.split(separator: "/").last!
+        return (file.hasPrefix("IMG") || file.hasPrefix("img")) && (file.hasSuffix(".JPG") || file.hasSuffix(".jpg"))
+    }
+    //var yml: Yaml = Yaml(dictionaryLiteral: "images")
+    var imgList: [Yaml] = [Yaml]()
+    for path in imgs {
+        imgList.append(Yaml.string(path))
+    }
+    let ymlList = Yaml.array(imgList)
+    let ymlDict = Yaml.dictionary([Yaml.string("images") : ymlList])
+    let ymlStr = try! ymlDict.save()
+    print(outpath)
+    try! ymlStr.write(to: URL(fileURLWithPath: outpath), atomically: true, encoding: .utf8)
+}
+
+func generateStereoImageList(left ldir: String, right rdir: String, outpath: String = dirStruc.calibrationSettings + "/stereoImageList.yml") {
+    guard var limgs = try? FileManager.default.contentsOfDirectory(atPath: ldir), var rimgs = try? FileManager.default.contentsOfDirectory(atPath: rdir) else {
+        print("could not read contents of directory \(ldir) or \(rdir)")
+        return
+    }
+    
+    let filterIms: (String) -> Bool = { (_ filepath: String) in
+        let file = filepath.split(separator: "/").last!
+        return (file.hasPrefix("IMG") || file.hasPrefix("img")) && (file.hasSuffix(".JPG") || file.hasSuffix(".jpg"))
+    }
+    limgs = limgs.filter(filterIms)
+    rimgs = rimgs.filter(filterIms)
+    let mapNames: (String) -> String = {(_ fullpath: String) in
+        return String(fullpath.split(separator: "/").last!)
+    }
+    let lnames = limgs.map(mapNames)
+    let rnames = rimgs.map(mapNames)
+    let names = Set(lnames).intersection(rnames)
+    var imgList = [Yaml]()
+    for name in names {
+        imgList.append(Yaml(stringLiteral: "\(ldir)/\(name)"))
+        imgList.append(Yaml(stringLiteral: "\(rdir)/\(name)"))
+    }
+    let ymlList = Yaml.array(imgList)
+    let ymlDict = Yaml(dictionaryLiteral: (Yaml(stringLiteral: "images"), ymlList))
+    try! ymlDict.save().write(to: URL(fileURLWithPath: outpath), atomically: true, encoding: .utf8)
+}
+
+
+class CalibrationSettings {
+    let filepath: String
+    var yml: Yaml
+    
+    enum CalibrationMode: String {
+        case INTRINSIC, STEREO, PREVIEW
+    }
+    enum CalibrationPattern: String {
+        case CHESSBOARD, ARUCO_SINGLE, ARUCO_BOX
+    }
+    
+    enum Key: String {
+        case Mode, Calibration_Pattern, ChessboardSize_Width
+        case ChessboardSize_Height, ImageList_Filename
+        case ArucoConfig_Filename, IntrinsicInput_Filename
+        case IntrinsicOutput_Filename, ExtrinsicOutput_Filename
+        case UndistortedImages_Path, RectifiedImages_Path
+        case DetectedImages_Path, Calibrate_FixDistCoeffs
+        case Calibrate_FixAspectRatio, Calibrate_AssumeZeroTangentialDistortion
+        case Calibrate_FixPrincipalPointAtTheCenter
+        case Show_UndistortedImages, ShowRectifiedImages, Show_ArucoMarkerCoordinates
+        case Wait_NextDetecedImage, LivePreviewCameraID
+    }
+    
+    init(_ path: String) {
+        self.filepath = path
+        do {
+            let ymlStr = try String(contentsOfFile: self.filepath)
+            let tmp = try Yaml.load(ymlStr)
+            guard let dict = tmp.dictionary else {
+                throw YamlError.InvalidFormat
+            }
+            guard dict[Yaml.string("Settings")] != nil else {
+                throw YamlError.MissingRequiredKey
+            }
+            self.yml = dict[Yaml.string("Settings")]!
+        } catch let error {
+            print(error.localizedDescription)
+            fatalError()
+        }
+    }
+    
+    func set(key: Key, value: Yaml) {
+        guard var dict = self.yml.dictionary else { return }
+        dict[Yaml.string(key.rawValue)] = value
+        self.yml = Yaml.dictionary(dict)
+    }
+    
+    func get(key: Key) -> Yaml? {
+        guard var dict = self.yml.dictionary else { return nil }
+        return dict[Yaml.string(key.rawValue)]
+    }
+    
+    func save() {
+        let out = try! Yaml.dictionary([Yaml.string("Settings") : self.yml]).save()
+        try! out.write(to: URL(fileURLWithPath: filepath), atomically: true, encoding: .utf8)
+    }
 }
