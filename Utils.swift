49b3fe43432904201bdd695dc469e0920fa36aab
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2018-06-26 14:45:15 -0400

    added List<T> class, which implements a bare-bones doubly linked list.

--- Utils.swift
+++ Utils.swift
@@ -23,3 +23,68 @@ func makeDir(_ str: String) -> Void {
 }
 
 let lockFlags = CVPixelBufferLockFlags(rawValue: 0) // read & write
+
+
+
+// because the Swift standard library doesn't have a built-in linked list class,
+// I wrote a minimalistic one. i'll add to it as needed
+class List<T> {
+    private class ListNode<T> {
+        var head: T
+        var tail: ListNode<T>?
+        var parent: ListNode<T>?
+        
+        init(head: T, tail: ListNode<T>? = nil, parent: ListNode<T>? = nil) {
+            self.head = head
+            self.tail = tail
+            self.parent = parent
+            
+            tail?.parent = self
+            parent?.tail = self
+        }
+    }
+    
+    var count: Int = 0
+    private var _first: ListNode<T>? = nil
+    private weak var _last: ListNode<T>? = nil
+    
+    var first: T? {
+        get {
+            return _first?.head
+        }
+        set {
+            guard let newValue = newValue else {
+                return
+            }
+            _first?.head = newValue
+        }
+    }
+    var last: T? {
+        get {
+            return _last?.head
+        }
+        set {
+            guard let newValue = newValue else {
+                return
+            }
+            _last?.head = newValue
+        }
+    }
+    
+    func insertFirst(_ obj: T) {
+        _first = ListNode<T>(head: obj, tail: _first)
+        _last = _last ?? _first
+    }
+    
+    func popLast() -> T? {
+        let value = _last?.head
+        if _first === _last {
+            _first = nil
+            _last = nil
+        } else {
+            _last = _last?.parent
+            _last?.tail = nil
+        }
+        return value
+    }
+}
