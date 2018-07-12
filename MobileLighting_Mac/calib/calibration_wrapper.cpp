02e210439711cb6485165a60c3e993a564ad091f
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2018-07-12 09:00:00 -0400

    ?

--- MobileLighting_Mac/calib/calibration_wrapper.cpp
+++ MobileLighting_Mac/calib/calibration_wrapper.cpp
@@ -8,8 +8,16 @@
 
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 int calibrateWithSettings(std::string);  // this is the function in calibrate.cpp
 extern "C" int CalibrateWithSettings(const char *inputSettingsFile) {   // this is the wrapped function for bridging to swift
     return calibrateWithSettings(std::string(inputSettingsFile));
+}
+
+std::vector<int> detectionCheck(char *inputSettingsFilepath, char *imleftpath, char *imrightpath);
+extern "C" int DetectionCheck(char *inputSettingsFile, char *imleft, char *imright) {
+    printf("inputsettings=%s, imleft=%s, imright=%s", inputSettingsFile, imleft, imright);
+    std::vector<int> result = detectionCheck(inputSettingsFile, imleft, imright);
+    return -1;
 }
