1ab18ad4cd1cea060e0c6dd0a0ddc0dd3e498211
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2018-07-12 09:01:48 -0400

    updated to reflect changes in functions

--- MobileLighting_Mac/activeLighting/activeLighting_wrapper.cpp
+++ MobileLighting_Mac/activeLighting/activeLighting_wrapper.cpp
@@ -7,15 +7,27 @@
 //
 
 #include <stdio.h>
+#include <iostream>
 #include "ImgProcessor.h"
 #include "Rectify.hpp"
 #include "Utils.h"
+#include "Disparities.h"
+#include <assert.h>
 
-extern "C" void refineDecodedIm(char *outdir, int direction, char* decodedIm, double angle) {
-    refine(outdir, direction, decodedIm, angle);	// returns final CFloatImage, ignore
+extern "C" void refineDecodedIm(char *outdir, int direction, char* decodedIm, double angle, char *posID) {
+    refine(outdir, direction, decodedIm, angle, posID);	// returns final CFloatImage, ignore
 }
 
-extern "C" void disparitiesOfRefinedImgs(char *posdir0, char *posdir1, char *outdir0, char *outdir1, int pos0, int pos1, int dXmin, int dXmax, int dYmin, int dYmax) {
+extern "C" void computeMaps(char *impath, char *intr, char *extr) {
+    printf("%s\n%s\n%s\n", impath, intr, extr);
+    CFloatImage im;
+    ReadImage(im, impath);
+    CShape sh = im.Shape();
+    printf("decoded image dimensions: [%d x %d]\n", sh.width, sh.height);
+    computemaps(sh.width, sh.height, intr, extr);
+}
+
+extern "C" void disparitiesOfRefinedImgs(char *posdir0, char *posdir1, char *outdir0, char *outdir1, int pos0, int pos1, int rectified, int dXmin, int dXmax, int dYmin, int dYmax) {
     // in0, in1 are flo images, need to create
     // so inputs should be to directories?
     int verbose = 1;
@@ -25,25 +37,28 @@ extern "C" void disparitiesOfRefinedImgs(char *posdir0, char *posdir1, char *out
     CFloatImage fdisp0, fdisp1;
     char filename[1000]; //, in0[1000], in1[1000];
     
-    // first create necessary FLO files for computeDisparities()
-    sprintf(filename, "%s/result0-4refined2.pfm", posdir0);
-    ReadImageVerb(x, filename, 0);
-    sprintf(filename, "%s/result1-4refined2.pfm", posdir0);
-    ReadImageVerb(y, filename, 0);
-    merged0 = mergeToFloImage(x, y);
-    //sprintf(filename, "%s/result.flo", posdir0);
-    //WriteFlowFile(merged0, filename);
-    //strcpy(in0, filename);
+    char leftID[50], rightID[50];
+    if (rectified) {
+        sprintf(leftID, "%d%d", pos0, pos1);
+        sprintf(rightID, "%d%d", pos0, pos1);
+    } else {
+        sprintf(leftID, "%d", pos0);
+        sprintf(rightID, "%d", pos1);
+    }
     
-    sprintf(filename, "%s/result0-4refined2.pfm", posdir1);
+    // first create necessary FLO files for computeDisparities()
+    sprintf(filename, "%s/result%su-4refined2.pfm", posdir0, leftID);
+    ReadImageVerb(x, filename, 1);
+    sprintf(filename, "%s/result%sv-4refined2.pfm", posdir0, leftID);
+    ReadImageVerb(y, filename, 1);
+    merged0 = mergeToFloImage(x, y);
+
+    sprintf(filename, "%s/result%su-4refined2.pfm", posdir1, rightID);
     ReadImageVerb(x, filename, 0);
-    sprintf(filename, "%s/result1-4refined2.pfm", posdir1);
+    sprintf(filename, "%s/result%sv-4refined2.pfm", posdir1, rightID);
     ReadImageVerb(y, filename, 0);
     merged1 = mergeToFloImage(x, y);
-    //sprintf(filename, "%s/result.flo", posdir1);
-    //WriteFlowFile(merged1, filename);
-    //strcpy(in1, filename);
-    
+
     computeDisparities(merged0, merged1, fdisp0, fdisp1, dXmin, dXmax, dYmin, dYmax);
     
     // now need to separate L(fdisp(0|1)) into u,v files corresponding to x-, y- disparities.
@@ -58,10 +73,10 @@ extern "C" void disparitiesOfRefinedImgs(char *posdir0, char *posdir1, char *out
     fy1 = ppos1.second;
     
     char px0[100], py0[100], px1[100], py1[100];
-    sprintf(px0, "%s/disp%d%dx.pfm", outdir0, pos0, pos1);
-    sprintf(py0, "%s/disp%d%dy.pfm", outdir0, pos0, pos1);
-    sprintf(px1, "%s/disp%d%dx.pfm", outdir1, pos1, pos0);
-    sprintf(py1, "%s/disp%d%dy.pfm", outdir1, pos1, pos0);
+    sprintf(px0, "%s/disp%d%dx-0initial.pfm", outdir0, pos0, pos1);
+    sprintf(py0, "%s/disp%d%dy-0initial.pfm", outdir0, pos0, pos1);
+    sprintf(px1, "%s/disp%d%dx-0initial.pfm", outdir1, pos0, pos1);
+    sprintf(py1, "%s/disp%d%dy-0initial.pfm", outdir1, pos0, pos1);
     
     WriteImageVerb(fx0, px0, verbose);
     WriteImageVerb(fy0, py0, verbose);
@@ -69,16 +84,97 @@ extern "C" void disparitiesOfRefinedImgs(char *posdir0, char *posdir1, char *out
     WriteImageVerb(fy1, py1, verbose);
 }
 
-//void rectifyDecoded(int nimages, char* destdir, char** matrices, char** photos);
-/*
-extern "C" void rectifyPFMs {
-    rectifyDecoded(nimages, camera, destdir, matrices, images);
-}*/
-extern "C" void computeMaps(char *impath, char *intr, char *extr) {
-    printf("%s\n%s\n%s\n", impath, intr, extr);
-    CFloatImage im;
-    ReadImage(im, impath);
-    CShape sh = im.Shape();
-    printf("decoded image dimensions: [%d x %d]\n", sh.width, sh.height);
-    computemaps(sh.width, sh.height, intr, extr);
+extern "C" void crosscheckDisparities(char *posdir0, char *posdir1, int pos0, int pos1, float thresh, int xonly, int halfocc, char *in_suffix, char *out_suffix) {
+    CFloatImage x0,x1,y0,y1;
+    char buffer[100];
+    sprintf(buffer, "%s/disp%d%dx-%s.pfm", posdir0, pos0, pos1, in_suffix);
+    ReadImageVerb(x0, buffer, 1);
+    sprintf(buffer, "%s/disp%d%dy-%s.pfm", posdir0, pos0, pos1, in_suffix);
+    ReadImageVerb(y0, buffer, 1);
+    sprintf(buffer, "%s/disp%d%dx-%s.pfm", posdir1, pos0, pos1, in_suffix);
+    ReadImageVerb(x1, buffer, 1);
+    sprintf(buffer, "%s/disp%d%dy-%s.pfm", posdir1, pos0, pos1, in_suffix);
+    ReadImageVerb(y1, buffer, 1);
+    
+    CFloatImage d0 = mergeToFloImage(x0, y0);
+    CFloatImage d1 = mergeToFloImage(x1, y1);
+    pair<CFloatImage,CFloatImage> outputs = runCrossCheck(d0, d1, thresh, xonly, halfocc);
+    pair<CFloatImage,CFloatImage> crosscheck0, crosscheck1;
+    crosscheck0 = splitFloImage(outputs.first);
+    crosscheck1 = splitFloImage(outputs.second);
+    
+    CFloatImage ccx0, ccy0, ccx1, ccy1;
+    ccx0 = crosscheck0.first;
+    ccx1 = crosscheck1.first;
+    ccy0 = crosscheck0.second;
+    ccy1 = crosscheck1.second;
+    
+    sprintf(buffer, "%s/disp%d%dx-%s.pfm", posdir0, pos0, pos1, out_suffix);
+    WriteImageVerb(ccx0, buffer, 1);
+    sprintf(buffer, "%s/disp%d%dy-%s.pfm", posdir0, pos0, pos1, out_suffix);
+    WriteImageVerb(ccy0, buffer, 1);
+    sprintf(buffer, "%s/disp%d%dx-%s.pfm", posdir1, pos0, pos1, out_suffix);
+    WriteImageVerb(ccx1, buffer, 1);
+    sprintf(buffer, "%s/disp%d%dy-%s.pfm", posdir1, pos0, pos1, out_suffix);
+    WriteImageVerb(ccy1, buffer, 1);
+}
+
+// CFloatImage runFilter(CFloatImage img, float ythresh, int kx, int ky, int mincompsize, int maxholesize);
+extern "C" void filterDisparities(char *dispx, char *dispy, char *outx, char *outy, int pos0, int pos1, float ythresh, int kx, int ky, int mincompsize, int maxholesize) {
+    assert (dispx != NULL);
+    assert (outx != NULL);
+    
+    CFloatImage x, y;
+    ReadImageVerb(x, dispx, 1);
+    if (dispy == NULL) {
+        y.ReAllocate(x.Shape());
+        y.FillPixels(INFINITY);
+    } else {
+        ReadImageVerb(y, dispy, 1);
+    }
+    CFloatImage merged = mergeToFloImage(x, y);
+    
+    CFloatImage mergedResult = runFilter(merged, ythresh, kx, ky, mincompsize, maxholesize);
+    pair<CFloatImage,CFloatImage> imgpair = splitFloImage(mergedResult);
+    x = imgpair.first;
+    y = imgpair.second;
+    
+    WriteImageVerb(x, outx, 1);
+    if (outy != NULL)
+        WriteImageVerb(y, outy, 1);
+}
+
+//CFloatImage mergeDisparityMaps(CFloatImage images[], int count, int mingroup, float maxdiff)
+extern "C" void mergeDisparities(char *imgsx[], char *imgsy[], char *outx, char *outy, int count, int mingroup, float maxdiff) {
+    CFloatImage images[count];
+    for (int i = 0; i < count; ++i) {
+        CFloatImage x, y, flo;
+        ReadImageVerb(x, imgsx[i], 1);
+        ReadImageVerb(y, imgsy[i], 1);
+        flo = mergeToFloImage(x, y);
+        images[i] = flo;
+    }
+    CFloatImage result = mergeDisparityMaps(images, count, mingroup, maxdiff);
+    pair<CFloatImage,CFloatImage> flo = splitFloImage(result);
+    WriteImageVerb(flo.first, outx, 1);
+    WriteImageVerb(flo.second, outy, 1);
+//    WriteImageVerb(result, out, 1);
+}
+
+//CFloatImage reproject(CFloatImage dispflo, CFloatImage codeflo, char* outFile, char* errFile, char* matfile);
+extern "C" void reprojectDisparities(char *dispx_file, char *dispy_file, char *codex_file, char *codey_file, char *outx_file, char *outy_file, char *err_file, char *mat_file, char *log_file) {
+    CFloatImage dispx, dispy, disp;
+    CFloatImage codex, codey, code;
+    CFloatImage outx, outy, out;
+    ReadImageVerb(dispx, dispx_file, 1);
+    ReadImageVerb(dispy, dispy_file, 1);
+    ReadImageVerb(codex, codex_file, 1);
+    ReadImageVerb(codey, codey_file, 1);
+    disp = mergeToFloImage(dispx, dispy);
+    code = mergeToFloImage(codex, codey);
+    
+    CFloatImage floresult = reproject(disp, code, err_file, mat_file, log_file);
+    pair<CFloatImage,CFloatImage> splitresult = splitFloImage(floresult);
+    WriteImageVerb(splitresult.first, outx_file, 1);
+    WriteImageVerb(splitresult.second, outy_file, 1);
 }
