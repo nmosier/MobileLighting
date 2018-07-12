11807320a69829c1d122526a57eafb11d847c8d6
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2018-07-12 09:03:09 -0400

    this file #define all parameters for image processing pipeline, across Swift & C++

---
+++ Parameters.h
@@ -0,0 +1,18 @@
+//
+//  Parameters.h
+//  MobileLighting
+//
+//  Created by Nicholas Mosier on 7/10/18.
+//  Copyright © 2018 Nicholas Mosier. All rights reserved.
+//
+
+//#ifndef Parameters_h
+//#define Parameters_h
+
+
+#define MERGE2_MAXDIFF (float) 1.0
+
+
+
+
+//#endif /* Parameters_h */
