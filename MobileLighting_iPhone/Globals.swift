0ececce803ab1a411a003d05d854ca846f0ad6c4
Nicholas Mosier <nicholas@Nicholass-MacBook-Air.local>
2018-07-21 11:03:26 -0400

    I didn't change anything.

--- MobileLighting_iPhone/Globals.swift
+++ MobileLighting_iPhone/Globals.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import AVFoundation
 
 var cameraService: CameraService!
 var cameraController = CameraController()
@@ -16,3 +17,4 @@ var decoder: Decoder?
 var motionRecorder = MotionRecorder()
 
 var orientation: AVCaptureVideoOrientation!
+var orientationObserver: NSKeyValueObservation!
